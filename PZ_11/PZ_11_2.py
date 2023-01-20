1db2ee7633e360f6ae4407ac1adfa5561b322541
Sofulya <goroj.son@mail.ru>
2023-01-20 16:18:10 +0300

    Commit PZ

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -11,9 +11,8 @@ print(f"Количество букв в нижнем регистре: {count_l
 file.close()
 file = open('new_text18-7.txt', 'w', encoding='UTF-8')
 
-rows_from_one_to_second = data.splitlines()[:2]
+rows_from_one_to_second = data.splitlines()[:2]  # возвращаем строчки, первую и вторую
 last_row = data.splitlines()[-1]
 mid_rows = data.splitlines()[2:6]
 print('\n'.join(rows_from_one_to_second) + '\n' + ''.join(last_row) + '\n' + '\n'.join(mid_rows), file=file)
 file.close()
-
