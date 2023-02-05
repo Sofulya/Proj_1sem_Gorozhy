e236cd04562b001de213c56575eb2d3f5b54f138
Sofulya <goroj.son@mail.ru>
2023-02-05 13:03:20 +0300

    Commit PZ

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -3,11 +3,15 @@
 from random import randint
 
 #  Заносим в переменные параметры матрицы
-a, b, c, d = [int(input(i)) for i in ("Количество строк: ", "Количество столбцов: ", "От = ", "До = ")]
+a, b, c, d = [int(input(i)) for i in ("Количество строк: ", "Количество столбцов: ", "От: ", "До: ")]
 #  Заполнение матрицы случайными числами
 matrix = [[randint(c, d) for _ in range(b)] for j in range(a)]
-print("Исходная матрица: ", '\n', [i for i in matrix])
+print(f"Исходная матрица: ")
+for i in matrix:
+    print(*i, sep='\t')
 #  Замена последних элементов в строке на -1
 for i in range(a):
     matrix[i][b - 1] = -1
-print("Полученная матрица: ", '\n', [i for i in matrix])
+print(f"Полученная матрица: ")
+for i in matrix:
+    print(*i, sep='\t')
