5a456e0d9fae77b170d4d6b92227461774878d17
Sofulya <goroj.son@mail.ru>
2023-03-10 18:05:10 +0300

    Commit PZ

--- PZ_16/figures/triangle/code.py
+++ PZ_16/figures/triangle/code.py
@@ -2,8 +2,12 @@ a, b, c = 7, 2, 8
 
 
 def triangle_perimeter(x=a, y=b, z=c):
+    """Вычисляет периметр треугольника.
+    Необходимы 3 стороны - 3 значения."""
     return x + y + z
 
 
 def triangle_area(x=a, y=b, z=c):
-    return (p * (p - x) * (p - y) * (p - z)) ** 0.5 if (p := (x + y + z) / 2) else 0
+    """Вычисляет площадь треугольника.
+    Необходимы 3 стороны - 3 значения."""
+    return (s * (s - x) * (s - y) * (s - z)) ** 0.5 if (s := (x + y + z) / 2) else 0
