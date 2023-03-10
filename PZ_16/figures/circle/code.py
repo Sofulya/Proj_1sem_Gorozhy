4383efab993a175485f6be7c1bc0306f5d2a6264
Sofulya <goroj.son@mail.ru>
2023-03-10 18:00:08 +0300

    Commit PZ

--- PZ_16/figures/circle/code.py
+++ PZ_16/figures/circle/code.py
@@ -2,8 +2,12 @@ default_radius = 5
 
 
 def circle_perimeter(r=default_radius):
+    """Вычисляет длину окружности.
+    Необходимо значение радиуса."""
     return 2 * 3.14 * r
 
 
 def circle_area(r=default_radius):
+    """Вычисляет площадь окружности.
+    Необходимо значение радиуса."""
     return 3.14 * r ** 2
---
+++ PZ_16/main.py
@@ -0,0 +1,7 @@
+from figures import circle_perimeter, circle_area
+from figures import square_perimeter, square_area
+from figures import triangle_perimeter, triangle_area
+
+print(circle_perimeter(10), circle_area())
+print(square_perimeter(), square_area(10))
+print(triangle_perimeter(1, 2, 3), triangle_area())
